e4827cebab43e5e3d33fc39e9a0e007c8e789f4a
Joel Plotnik <joelplotnik@gmail.com>
2022-07-22 11:13:06 -0700

    Implemented login

--- script.js
+++ script.js
@@ -79,33 +79,30 @@ const displayMovements = function (movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
-displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = `${balance}€`;
 };
-calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)}€`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposit => (deposit * 1.2) / 100)
+    .map(deposit => (deposit * acc.interestRate) / 100)
     .filter(int => int >= 1)
     .reduce((acc, int) => acc + int, 0);
   labelSumInterest.textContent = `${interest}€`;
 };
-calcDisplaySummary(account1.movements);
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -118,23 +115,31 @@ const createUsernames = function (accs) {
 };
 createUsernames(accounts);
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+// Event Handler
+let currentAccount;
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault(); // prevent form from submitting
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
 
-const euroToUsd = 1.1;
+  // optional chaining (?) only returns a value if it exists (avoids error)
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // display UI and wlecome message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
 
-// PIPELINE
-const totalDepositUSD = movements
-  .filter(mov => mov > 0)
-  .map(mov => mov * euroToUsd)
-  .reduce((acc, mov) => acc + mov, 0);
-console.log(totalDepositUSD);
+    // clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur(); /// makes it so the field loses it's focus
+
+    displayMovements(currentAccount.movements); // dispaly movements
+    calcDisplayBalance(currentAccount.movements); // display balance
+    calcDisplaySummary(currentAccount); // display summary
+  }
+});
